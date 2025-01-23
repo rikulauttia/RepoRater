b33248948eae45849294867001ac403e63ab5912
Riku Lauttia <riku.lauttia@gmail.com>
2025-01-23 19:47:20 +0200

    exercise 10.16 completed

--- frontend/src/components/AppBar.jsx
+++ frontend/src/components/AppBar.jsx
@@ -2,6 +2,10 @@ import Constants from "expo-constants";
 import { ScrollView, StyleSheet, View } from "react-native";
 import { useNavigate } from "react-router-native";
 
+import { useApolloClient, useQuery } from "@apollo/client";
+
+import { ME } from "../graphql/queries";
+import useAuthStorage from "../hooks/useAuthStorage";
 import theme from "../theme";
 import AppBarTab from "./AppBarTab";
 
@@ -9,7 +13,6 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight + 10,
     backgroundColor: theme.colors.appBarBackground,
-    paddingBottom: 10,
     paddingBottom: 10,
     paddingHorizontal: 15,
   },
@@ -20,12 +23,28 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const { data } = useQuery(ME);
+  const authStorage = useAuthStorage();
   const navigate = useNavigate();
+  const apolloClient = useApolloClient();
+
+  const signOut = async () => {
+    await authStorage.removeAccessToken();
+    await apolloClient.resetStore();
+    navigate("/signin");
+  };
+
+  const isLoggedIn = data?.me;
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal contentContainerStyle={styles.scrollContainer}>
         <AppBarTab title="Repositories" onPress={() => navigate("/")} />
-        <AppBarTab title="Sign In" onPress={() => navigate("/signin")} />
+        {isLoggedIn ? (
+          <AppBarTab title="Sign Out" onPress={signOut} />
+        ) : (
+          <AppBarTab title="Sign In" onPress={() => navigate("/signin")} />
+        )}
       </ScrollView>
     </View>
   );
